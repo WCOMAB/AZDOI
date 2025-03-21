32a18c9ce5577751a564119d93fa995177a5214a
Elie Bou Absi <eliabs@wcom.se>
2025-03-21 11:18:15 +0100

    Mermaid diagram extension, run in parallel test

--- src/AZDOI/Extensions/MermaidDiagramExtensions.cs
+++ src/AZDOI/Extensions/MermaidDiagramExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace AZDOI.Extensions; // ✅ Use an appropriate namespace
+﻿namespace AZDOI.Extensions;
 
 public static class MermaidDiagramExtensions
 {
@@ -8,31 +8,21 @@ public static class MermaidDiagramExtensions
 
         sb.AppendLine("```mermaid");
         sb.AppendLine("graph TD");
-        sb.AppendLine($"    Org_{organization.Id}({organization.Name})");
+        sb.AppendLine($"Org_{organization.Id}({organization.Name})");
 
         foreach (var project in organization.Children)
         {
             sb.AppendLine();
-            sb.AppendLine($"    %% {project.Name} project");
-            sb.AppendLine($"    subgraph Proj_{project.Id}[{project.Name}]");
-
-            if (project.Children.Any()) 
+            sb.AppendLine($"%% {project.Name} project");
+            sb.AppendLine($"subgraph Proj_{project.Id}[{project.Name}]");
+            sb.AppendLine($"subgraph Repos_{project.Id}[Repositories]");
+            foreach (var repo in project.Children)
             {
-                sb.AppendLine($"        subgraph Repos_{project.Id}[Repositories]");
-                foreach (var repo in project.Children)
-                {
-                    sb.AppendLine($"            Repo_{project.Id}_{repo.Id}[{repo.Name}]");
-                }
-                sb.AppendLine("        end");
+                sb.AppendLine($"Repo_{project.Id}_{repo.Id}[{repo.Name}]");
             }
-            else
-            {
-                sb.AppendLine("        %% No repositories found in this project");
-            }
-
-            sb.AppendLine("    end");
-
-            sb.AppendLine($"    Org_{organization.Id} --> Proj_{project.Id}");
+            sb.AppendLine("end"); 
+            sb.AppendLine("end"); 
+            sb.AppendLine($"Org_{organization.Id} --> Proj_{project.Id}");
         }
 
         sb.AppendLine("```");
