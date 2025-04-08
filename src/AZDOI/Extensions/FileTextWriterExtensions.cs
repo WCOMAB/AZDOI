51553e82c4bb46db31fc32c29090724e26469f4b
Mattias Karlsson <matkar@wcom.se>
2025-04-08 15:39:26 +0200

    feat: Add YamlDotNet for improved frontmatter serialization and fix tooltip escaping

--- src/AZDOI/Extensions/FileTextWriterExtensions.cs
+++ src/AZDOI/Extensions/FileTextWriterExtensions.cs
@@ -4,7 +4,7 @@ public static class FileTextWriterExtensions
     public static async Task WriteClickNode(this FileTextWriter writer, string nodeId, string relativeUrl, string description)
     {
         var escapedUrl = relativeUrl.PathEscapeUriString();
-        var line = $"            click {nodeId} href \"{escapedUrl}\" \"{description}\"";
+        var line = $"            click {nodeId} href \"{escapedUrl}\" {JsonSerializer.Serialize(description)}";
         await writer.WriteLineAsync(line);
     }
 }
